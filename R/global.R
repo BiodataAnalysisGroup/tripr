506e098f246a79578cc3db152f6c6ce11aac95e8
iofeidis <jasonof75@gmail.com>
2021-08-26 14:42:44 +0300

    Change logFile place

--- R/global.R
+++ R/global.R
@@ -47,10 +47,8 @@ tmp_path <- getwd() # change it to "/tmp" for server
 num_of_cores <- parallel::detectCores(all.tests = FALSE, logical = TRUE) # change this to the custom number of threads
 
 # logfile
-if (!file.exists(paste0(tmp_path, "/inst/extdata/log_files"))) {
-    fs::dir_create(paste0(tmp_path, "/inst/extdata/log_files"))
-}
-logFile <- paste0(tmp_path, "/inst/extdata/log_files/log_file_", trunc(as.numeric(Sys.time())), ".txt")
+fs::dir_create(paste0(fs::path_package("extdata", package="tripr"), "/log_files"))
+logFile <- paste0(fs::path_package("extdata", "log_files", package="tripr"), "/log_file_", trunc(as.numeric(Sys.time())), ".txt")
 cat(paste0("Function", "\t", "Parameters", "\t", "Num of input rows", "\t", "Num of input columns", "\t", "Start time", "\t", "End time", "\t", "Memory used"), file = logFile, append = FALSE, sep = "\n")
 
 use_only_useful_columns <- TRUE # libraries.R TRUE
